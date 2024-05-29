f3fdbb4ff1abcf73a0d1e61fbee34de759ec0aeb
NSimonet2312 <pr65snl@eduvaud.ch>
2024-05-29 08:33:27 +0200

    Initialisation du projet

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('fruit-calc-cache').then((cache) => {
+        return cache.addAll([
+          '/calculateur-fruit/index.html',
+          '/calculateur-fruit/style.css',
+          '/calculateur-fruit/bootstrap-5.3.3-dist/css/bootstrap.min.css'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
